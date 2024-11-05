c7a4060302a7e49aead1bbc0154b1594bb69c184
adamgado <adamgado33@gmail.com>
2024-11-05 17:58:49 +0000

    1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""basic Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """configure available languages"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """basic Flask app"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
